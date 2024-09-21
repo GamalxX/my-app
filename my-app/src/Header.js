bea1a01e13fd717669fff28fda3e83e7a57aa6c2
GamalxX <gamalns2001@gmail.com>
2024-09-21 21:52:09 +0300

    Add meta tags and update Open Graph setup

---
+++ my-app/src/Header.js
@@ -0,0 +1,8 @@
+function Header(){
+    return (
+      <header>
+        <img src="path_to_logo" alt="Little Lemon Logo" />
+      </header>
+    );
+}
+export default Header;
---
+++ my-app/src/Nav.js
@@ -0,0 +1,21 @@
+function Nav() {
+  return (
+    <Nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">about</a>
+        </li>
+        <li>
+          <a href="/menu">Menu</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </Nav>
+  );
+}
+export default Nav;
