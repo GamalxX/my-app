cf2a27e1edb3de8296e05832b1115ff3f884dd81
GamalxX <gamalns2001@gmail.com>
2024-10-02 16:44:50 +0300

    Updated project with new files and configurations

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,32 @@
-import './App.css';
-import Header from "./Header";
-import Nav from "./Nav";
-import Main from "./Main";
-import Footer from "./Footer";
+import React, { Suspense, lazy, useEffect } from "react";
+import Loading from "./components/Loading";
+import Lenis from "@studio-freight/lenis";
 
-function App() {
+const Header = lazy(() => import("./components/Header"));
+const AppRoutes = lazy(() => import("./routes/AppRoutes"));
+const Footer = lazy(() => import("./components/Footer"));
+
+const App = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <>
-      <Header />
-      <Nav />
-      <Main />
-      <Footer />
+      <Suspense fallback={<Loading />}>
+        <Header />
+        <AppRoutes />
+        <Footer />
+      </Suspense>
     </>
   );
-}
+};
 
 export default App;
